5c8f77974088bc3ac655feb35bef48dbc05c2782
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-09-18 10:38:42 +0200

    py

--- .ci/jenkins/conf.py
+++ .ci/jenkins/conf.py
@@ -8,8 +8,7 @@ winpylocation = {"py27": "C:\\Python27\\python.exe",
                  "py36": "C:\\Python36\\python.exe"}
 
 macpylocation = {"py27": "/usr/bin/python",  # /Users/jenkins_ci/.pyenv/versions/2.7.11/bin/python",
-                 "py34": "/Users/jenkins_ci/.pyenv/versions/3.4.7/bin/python",
-                 "py36": "/Users/jenkins_ci/.pyenv/versions/3.6.5/bin/python"}
+                 "py37": "/usr/bin/python3"}
 
 linuxpylocation = {"py27": "/usr/bin/python2.7",
                    "py34": "/usr/bin/python3.4",
