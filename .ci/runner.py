1c23f925c0de918d6af3ae73817ae15460d444e5
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-09-18 18:03:43 +0200

    timer

--- .ci/runner.py
+++ .ci/runner.py
@@ -46,7 +46,7 @@ def run_tests(module_path, pyver, source_folder, tmp_folder, flavor,
         pip_installs += "pip install -r conans/requirements_osx.txt && "
 
     #  --nocapture
-    timer = "--with-timer"
+    timer = "--with-timing"
     command = "virtualenv --python \"{pyenv}\" \"{venv_dest}\" && " \
               "{source_cmd} \"{venv_exe}\" && " \
               "{pip_installs} " \
